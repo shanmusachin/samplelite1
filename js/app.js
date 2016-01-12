3df4f1f4bc3062b3d57f582ecd23ebbac6649864
shanmusachin <shanmusachin@gmail.com>
2016-01-12 16:08:57 -0600

    changes

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 'use strict';
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('sw.js', { scope: '/sample/' }).then(function(reg) {
+  navigator.serviceWorker.register('sw.js', { scope: '/samplelite1/' }).then(function(reg) {
     // registration worked
     console.log('Registration succeeded. Scope is ' + reg.scope);
   }).catch(function(error) {
