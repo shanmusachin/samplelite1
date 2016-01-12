e36ae256ae59560c1467c0198b1c45f17a26cbe0
shanmusachin <shanmusachin@gmail.com>
2016-01-12 15:57:07 -0600

    First pages commit

---
+++ js/app.js
@@ -0,0 +1,10 @@
+'use strict';
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('sw.js', { scope: '/sample/' }).then(function(reg) {
+    // registration worked
+    console.log('Registration succeeded. Scope is ' + reg.scope);
+  }).catch(function(error) {
+    // registration failed
+    console.log('Registration failed with ' + error);
+  });
+};
---
+++ sw.js
@@ -0,0 +1,22 @@
+this.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open('waglite_cache').then(function(cache) {
+      return cache.addAll([ ]);
+    })
+  );
+  console.log('Installed', event);
+});
+
+
+this.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.open('waglite_cache').then(function(cache) {
+      return cache.match(event.request).then(function (response) {
+        return response || fetch(event.request).then(function(response) {
+          cache.put(event.request, response.clone());
+          return response;
+        });
+      });
+    })
+  );
+});
